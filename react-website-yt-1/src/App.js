9366fc3e53c758ae17b1c5f22997276944ef3b5f
prasanth <kvsprasanth007@gmail.com>
2025-03-14 00:24:50 -0400

    fix: correct typo in message in App.js

--- react-website-yt-1/src/App.js
+++ react-website-yt-1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload prasanth.
+          Edit <code>src/App.js</code> and save to  prasanth.
         </p>
         <a
           className="App-link"
